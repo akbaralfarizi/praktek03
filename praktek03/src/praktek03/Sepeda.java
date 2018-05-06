2ab694d3c95677363b05f4e5327f20bcf864d5ed
Akhmad Akbar Alfarizi <32434489+akbaralfarizi@users.noreply.github.com>
2018-05-06 21:52:28 +0700

    Membuat Method CetakInfo()

--- praktek03/src/praktek03/Sepeda.java
+++ praktek03/src/praktek03/Sepeda.java
@@ -1,5 +1,13 @@
 package praktek03;
 
 public class Sepeda {
+    string merk;
+    string warna;
+    double harga;
     
+    void cetakinfo(){
+        System.out.println("Merk \t: "+merk+"\n"+
+                "Warna \t: "+warna+"\n"+
+                "Harga \t: "+harga);
+    }
 }
