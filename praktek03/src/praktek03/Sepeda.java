c0d2257274bc9eee06d548db46a4ba2ec3ce7975
Akhmad Akbar Alfarizi <32434489+akbaralfarizi@users.noreply.github.com>
2018-05-06 22:14:55 +0700

    Instance, buat Atribut dan panggil Method

--- praktek03/src/praktek03/Sepeda.java
+++ praktek03/src/praktek03/Sepeda.java
@@ -5,9 +5,13 @@ public class Sepeda {
     string warna;
     double harga;
     
-    void cetakinfo(){
+    void cetakInfo(){
         System.out.println("Merk \t: "+merk+"\n"+
                 "Warna \t: "+warna+"\n"+
                 "Harga \t: "+harga);
+    }
+
+    void cetakinfo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
